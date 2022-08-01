cb345a5503a09a3a2b739e26ababfc6d2921caa2
Samin Yeaser Rahman <60929012+PrinceMad@users.noreply.github.com>
2022-08-01 13:05:25 +0600

    Update Reply.java

--- src/main/java/events/Reply.java
+++ src/main/java/events/Reply.java
@@ -27,32 +27,35 @@ public class Reply extends ListenerAdapter {
             e.getChannel().sendMessage("ho, shanto noob").queue();
         else if (a.equalsIgnoreCase("|marakha")||a.equalsIgnoreCase("|mara kha"))
             e.getChannel().sendMessage("Ghumaitesi, jalas na\nSong sunte chaile |join koray |play lekh").queue();
-        else if (a.equalsIgnoreCase("<@&767801627750826005>"))
-            e.getChannel().sendMessage("ajaira game").queue();
         else if (a.equalsIgnoreCase("<@&513390467925409833>"))
             e.getChannel().sendMessage("Outlaw is dead").queue();
         else if (a.equalsIgnoreCase("lol"))
             e.getChannel().sendMessage("L 0 L").queue();
         else if (a.equalsIgnoreCase("Rachel"))
             e.getChannel().sendMessage("Baler nam nibi na").queue();
-        else if (a.equalsIgnoreCase("<@!446335997085483029>")) //Tasin
+        else if (a.equalsIgnoreCase("<@446335997085483029>")) //Tasin
             e.getChannel().sendMessage("Ami boba, kotha bolbo na!").queue();
-        else if (a.equalsIgnoreCase("<@!387264216899387392>")) //Farhan
+        else if (a.equalsIgnoreCase("<@387264216899387392>")) //Farhan
             e.getChannel().sendMessage("আমি এখন দিশেহারা").queue();
-        else if (a.equalsIgnoreCase("<@!405769489808883712>")) //Saad
+        else if (a.equalsIgnoreCase("<@405769489808883712>")) //Saad
             e.getChannel().sendMessage("Amr ase 15, tomar ase koto?").queue();
-        else if (a.equalsIgnoreCase("<@!273355497783164928>")) //Ishman
+        else if (a.equalsIgnoreCase("<@273355497783164928>")) //Ishman
             e.getChannel().sendMessage("Pro-est gamer alive").queue();
-        else if (a.equalsIgnoreCase("<@!493855038796595200>")) //Shanto
+        else if (a.equalsIgnoreCase("<@493855038796595200>")) //Shanto
             e.getChannel().sendMessage("pola noob").queue();
-        else if (a.equalsIgnoreCase("<@!649299590650265610>")) //choto Shanto
+        else if (a.equalsIgnoreCase("<@649299590650265610>")) //choto Shanto
             e.getChannel().sendMessage("Nokol dot user").queue();
-        else if (a.equalsIgnoreCase("<@!419839150653440001>")) //boro Shanto
+        else if (a.equalsIgnoreCase("<@419839150653440001>")) //boro Shanto
             e.getChannel().sendMessage("Java is life").queue();
-        else if (a.equalsIgnoreCase("<@!435077413253152770>")) //ami
+        else if (a.equalsIgnoreCase("<@435077413253152770>")) //ami
             e.getChannel().sendMessage("ghumaitesi, jalash na").queue();
-        else if (a.equalsIgnoreCase("<@!885388273160192022>")) //bot
+        else if (a.equalsIgnoreCase("<@885388273160192022>")) //bot
             e.getChannel().sendMessage("ghumaitesi, jalash na").queue();
+        
+        else if (a.equalsIgnoreCase("<@640549288749039618>")) //sadman
+            e.getChannel().sendMessage("ak 69kg, beshi na vaya").queue();
+        else if (a.equalsIgnoreCase("<@432224139227430912>")) //sabit
+            e.getChannel().sendMessage("code kortesi, jalas na").queue();
         System.out.println(e.getMessage().getContentRaw());
     }
 }
